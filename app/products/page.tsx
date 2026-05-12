b2e4be2f3d5c3d3968bbd0ae3150f44c6e4dda25
erustamovv09-netizen <e.rustamovv.09@gmail.com>
2026-05-11 23:20:42 -0700

    Products sahifasiga cache no-store qo'shildi

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,15 @@
 export default async function Home() {
-    const res = await fetch("https://avtobozor.onrender.com/mashinalar/");
-    const data = await res.json();
+    // 1. Next.js ga "Buni build vaqtida qidirmagin, keshlamagin" deb aytamiz
+    const res = await fetch("https://avtobozor.onrender.com/mahsulot/", { 
+        cache: 'no-store' 
+    });
 
+    // 2. Agar Render uxlab yotgan bo'lsa, xato berib yiqilmasligi uchun himoya qo'yamiz
+    if (!res.ok) {
+        return <div className="text-center mt-10 text-xl font-bold text-red-500">Server uyg'onmoqda... 1 daqiqadan so'ng sahifani yangilang!</div>;
+    }
+
+    const data = await res.json();
 
     return (
         <div>

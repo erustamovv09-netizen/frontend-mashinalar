33f7a47d2c0fe8feca1f5f4a7b08edca307e6c92
erustamovv09-netizen <e.rustamovv.09@gmail.com>
2026-06-01 23:54:01 -0700

    togirlandi

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,6 @@ export default async function Home() {
                   </Badge>
                 </div>
               </Link>
-        
 
               <CardHeader>
                 <Link href={`/cars/${item.id}`}>
